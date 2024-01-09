c88b7a89b308e70a5c359cb0cf18bb93b735e743
Joydas46 <arkajoy.das@gmail.com>
2024-01-09 17:31:33 +0530

    Adding padding in between rows of the homescreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import {Row,Col} from 'react-bootstrap'    
+import { Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
 import axios from 'axios'
 
@@ -22,7 +22,7 @@ const HomeScreen = () => {
             {/* map uses {}, but we can also use other syntax. If we have multiple lines we w
             wrap it in () */}
             {products.map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3} className='my-3'>
                     <Product product={product}/>
                 </Col>
             ))}
