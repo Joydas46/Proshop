fcdd2ae688f38992a4c0419c880a4666c2cb1a71
Joydas46 <arkajoy.das@gmail.com>
2024-01-12 17:12:55 +0530

    Product Api Slice and get products endpoint and get productdetails endpoint

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,32 +1,42 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
 import { Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { useGetProductsQuery } from '../slices/productApiSlices'
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const {data: products, isLoading, error} = useGetProductsQuery()
+  // const [products, setProducts] = useState([])
 
-  useEffect(() => { 
-      const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  },[])
+  // useEffect(() => { 
+  //     const fetchProducts = async () => {
+  //     const {data} = await axios.get('/api/products')
+  //     setProducts(data)
+  //   }
+  //   fetchProducts()
+  // },[])
 
   return (
     <>
-        <h1>List Of Products</h1>
-        <Row>
-            {/* map uses {}, but we can also use other syntax. If we have multiple lines we w
-            wrap it in () */}
-            {products.map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3} className='my-3'>
-                    <Product product={product}/>
-                </Col>
-            ))}
-        </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>List Of Products</h1>
+          <Row>
+              {/* map uses {}, but we can also use other syntax. If we have multiple lines we w
+              wrap it in () */}
+              {products.map((product) => (
+                  <Col key={product._id} sm={12} md={6} lg={4} xl={3} className='my-3'>
+                      <Product product={product}/>
+                  </Col>
+              ))}
+          </Row>
+        </>
+      )}
     </>
   )
 }
