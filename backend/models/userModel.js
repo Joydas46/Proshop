60fc99e50f5f11180c567c8e73705d3b4a8f5aa5
Joydas46 <arkajoy.das@gmail.com>
2024-01-22 15:35:26 +0530

    Implementing get user profile and update user profile

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,6 +30,19 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// Hash the plain text password before saving to the Db. The method pre() is used when we want to do something 
+// before saving to the DB
+userSchema.pre('save', async function (next) {
+    // If password is not modified, move to the next middleware
+    // This can happen if we are updating the user profile and not changing the password
+    if(!this.isModified('password')) {
+        next()
+    }
+    // If password is modified, hash it
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
