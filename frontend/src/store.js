f790c9c4e350826e6c2006666df2129df3226d1d
Joydas46 <arkajoy.das@gmail.com>
2024-01-26 11:03:20 +0530

    Implementing auth slice and user slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,7 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { apiSlice } from "./slices/apiSlices"
 import cartSliceReducer from "./slices/cartSlice"
+import authSliceReducer from "./slices/authSlice"
 
 // Entry point for redux. A store is a JavaScript object with a few special functions and abilities that make 
 // it different than a plain global object.
@@ -9,6 +10,7 @@ const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         cart: cartSliceReducer,
+        auth: authSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
