02392309d59a4e7d09918651bb8a3b630de85eba
Joydas46 <arkajoy.das@gmail.com>
2024-01-31 20:16:13 +0530

    Adding add order items to protected route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -11,7 +11,7 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 // Only admin can access these
-router.route('/').post(addOrderItems).get(protect, admin, getOrders)
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
