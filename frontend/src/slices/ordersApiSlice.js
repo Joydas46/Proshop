21fd811598ac131f61733f80b95d439f940d3645
Joydas46 <arkajoy.das@gmail.com>
2024-02-03 11:41:34 +0530

    Adding the list of orders for each user in the profile

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -29,8 +29,15 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
                 url: PAYPAL_URL,
             }),
             keepUnusedDataFor: 5,
+        }),
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/myorders`,
+                method: "GET",
+            }),
+            keepUnusedDataFor: 5,
         })
     }),
 })
 
-export const {useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery} = ordersApiSlice
+export const {useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery, useGetMyOrdersQuery} = ordersApiSlice
