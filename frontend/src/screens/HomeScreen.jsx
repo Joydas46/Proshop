0c6588eb4175a9e4a01e32b2ee64ba49d7bdae23
Joydas46 <arkajoy.das@gmail.com>
2024-02-10 12:29:34 +0530

    implementing search products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -11,8 +11,8 @@ import Paginate from '../components/Paginate'
 // import axios from 'axios'
 
 const HomeScreen = () => {
-  const {pageNumber} = useParams()
-  const {data, isLoading, error} = useGetProductsQuery({pageNumber})
+  const {pageNumber, keyword} = useParams()
+  const {data, isLoading, error} = useGetProductsQuery({keyword,pageNumber})
   // const [products, setProducts] = useState([])
 
   // useEffect(() => { 
@@ -41,7 +41,7 @@ const HomeScreen = () => {
                   </Col>
               ))}
           </Row>
-          <Paginate pages={data.pages} page={data.page}/>
+          <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''}/>
         </>
       )}
     </>
